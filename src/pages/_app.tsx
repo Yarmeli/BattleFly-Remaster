664d2ab15a73c3c773b74132f1410d2a394e62f4
Yarmeli <yarmelimine@gmail.com>
2023-03-28 18:14:08 +0100

    added metadata, navbar and styling to the _app.tsx file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,28 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "@/styles/globals.css";
+import Head from "next/head";
+import NavbarComponent from "@/components/navbar";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>BattleFly - Play to Earn Web3 Strategy Game</title>
+        <meta
+          name="description"
+          content="BattleFly - Play to Earn Web3 Strategy Game"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col">
+        <NavbarComponent />
+        <main className="flex flex-grow flex-col items-center justify-center">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default MyApp;
