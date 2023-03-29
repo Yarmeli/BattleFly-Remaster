621ded1c590a51a38a7b22a73cc8f21de3034adf
Yarmeli <yarmelimine@gmail.com>
2023-03-29 03:01:17 +0100

    added join discord card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,12 @@ const Home: NextPage = () => {
   return (
     <>
       <HeroComponent />
+      <div className="w-full bg-base-100">
+        <div className="flex flex-row items-center justify-center gap-5 p-5 text-center">
+          <h1 className="text-xl">Ready to meet the community?</h1>
+          <button className="btn-outline btn font-bold">Join Discord</button>
+        </div>
+      </div>
     </>
   );
 };
