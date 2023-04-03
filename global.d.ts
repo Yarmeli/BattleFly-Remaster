2c206aef384dc8c262498b52d8e690e7c8b8d1d0
Yarmeli <yarmelimine@gmail.com>
2023-04-03 18:20:19 +0100

    fix issue with swiper container and swiper slide not being recognised as elements

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,18 @@
 declare module "daisyui/src/colors/themes";
+
+import type { SwiperSlideProps, SwiperProps } from "swiper/react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "swiper-container": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement> & SwiperProps,
+        HTMLElement
+      >;
+      "swiper-slide": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement> & SwiperSlideProps,
+        HTMLElement
+      >;
+    }
+  }
+}
