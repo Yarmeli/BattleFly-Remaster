61567f785915265bdc391dddb71d54c09313f89d
Yarmeli <yarmelimine@gmail.com>
2023-04-04 05:20:38 +0100

    improved swiper coverfow settings

--- src/components/battlegrounds/swiperCoverflow.tsx
+++ src/components/battlegrounds/swiperCoverflow.tsx
@@ -18,34 +18,16 @@ function SwiperCoverflowComponent({ images }: Props) {
         //   REMEMBER TO USE PARAMETERS AS "kebab-case"
         //   e.g. use "slides-per-view" instead of "slidesPerView"
         effect="coverflow"
-        centered-slides="true"
         grab-cursor="true"
-        slides-per-view="5"
-        loop={true}
+        slides-per-view="2"
+        initial-slide="1"
+        loop
         navigation
-        // autoplay
-        // allow the user to read the content
-        // coverflowEffect={{
-        //   depth: 1,
-        //   modifier: 1,
-        //   rotate: 1,
-        //   stretch: 1,
-        //   scale: 1,
-        //   slideShadows: false,
-        // }}
-        // autoplay-pause-on-mouse-enter="true"
         coverflow-effect-rotate="0"
-        coverflow-effect-stretch="150"
+        coverflow-effect-stretch="500"
         coverflow-effect-depth="200"
         coverflow-effect-modifier="1"
         coverflow-effect-slide-shadows="false"
-        // coverflowEffect={{
-        //   rotate: 0,
-        //   stretch: 150,
-        //   depth: 200,
-        //   modifier: 1,
-        //   slideShadows: false,
-        // }}
       >
         {images.map((image) => (
           <swiper-slide key={image.key}>{image}</swiper-slide>
